a5368679f8739f9d91f2108f8d87408a78930fb6
wparke4 <willparke4@gmail.com>
2021-10-26 13:27:53 -0700

    added to do list to README.md

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
+/*To-Do
+    1. enable user to toggle between read and not read
+        a) issue: user can currently only toggle once per book
+    2. Likely an issue of toggling once a book has removed 
+        since the status is linked to the index of the book
+        Once the index shifts from removing a book then the 
+        status will not longer be synced to the correct book
+    3. STYLE
+    4. create back end where this is all saved
+    5. allow users to sign in so that their book list is saved and
+        they can access it
+*/
 const addBookButton = document.getElementById('addBookButton');
 const addBookContainer = document.querySelector('.addBookContainer');
 
